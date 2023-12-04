918fecbb544a2d2a3313b3e8cbb1e23f6be3b23f
Lev Dudnichenko <dudlev730@gmail.com>
2023-12-04 21:03:41 +0300

    решил задачу  "Ротация Слайса"

--- slice2/main.go
+++ slice2/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+var (
+	ErrWrongNum = errors.New("wrong num, must be > 0")
+)
+
+func main() {
+	res, err := rotate([]int{1, 2, 3, 4, 5}, 1)
+	if err != nil {
+		fmt.Printf("func rotate error: [%v]", err)
+		return
+	}
+	fmt.Println(res)
+}
+
+func rotate(arr []int, num int) ([]int, error) {
+	if num < 0 {
+		return nil, ErrWrongNum
+	}
+	num = num % len(arr)
+
+	sl := make([]int, len(arr)) // len, cap?
+
+	for i := 0; i < len(arr); i++ {
+		if i+num >= len(arr) {
+			sl[i+num-len(arr)] = arr[i]
+			continue
+		}
+		sl[i+num] = arr[i]
+	}
+
+	return sl, nil
 }
