19edeec1dc6a41741d4f9a9e60c8fe3e29477112
Lev Dudnichenko <dudlev730@gmail.com>
2023-12-20 21:14:52 +0300

    Обработка ошибок, функция умножения

--- calc/main.go
+++ calc/main.go
@@ -4,10 +4,25 @@ import "fmt"
 
 func main() {
 	var a, b int
-	fmt.Scanf("%d%d", &a, &b)
-	fmt.Println(sum(a, b))
+	var c string
+	_, err := fmt.Scanf("%d%s%d", &a, &c, &b)
+	if err != nil {
+		fmt.Printf("Are you really stupid? Your error: [%v]\n", err)
+	}
+	if c == "+" {
+		fmt.Println(sum(a, b))
+	}
+	if c == "*" {
+		fmt.Println(mult(a, b))
+	}
 }
 
+// функция, складывающая два числа
 func sum(a, b int) int {
 	return a + b
+}
+
+// функция, умножащая 2 числа
+func mult(a, b int) int {
+	return a * b
 }
